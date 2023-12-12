ac84495c1b776ec07b93cc55c19804480197021e
hiluf.meressa@ienetworksolutions.com <hmkahsay@gmail.com>
2023-12-12 14:58:08 +0300

    Your descriptive commit message here

--- push.sh
+++ push.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
 
 # List of packages (directories) in your monorepo
-PACKAGES=("task-management-system-back-end" "task-management-system-front-end")
+PACKAGES=("task-management-system-front-end" "task-management-system-back-end")
 
 # Loop through each package
 for PACKAGE in "${PACKAGES[@]}"; do
     echo "Processing package: $PACKAGE"
+    # cd "packages/$PACKAGE" || exit
 
-    # Check status
-    git status
+    # Check if there are changes
+    if git diff --quiet; then
+        echo "No changes in package: $PACKAGE"
+    else
+        # Show changes
+        echo "Changes in package: $PACKAGE"
+        git diff
 
-    # Add changes
-    git add .
+        # Add changes
+        git add .
 
-    # Commit changes
-    git commit -m "Your descriptive commit message here"
+        # Commit changes
+        git commit -m "Your descriptive commit message here"
 
-    # Push changes
-    git push origin main
+        # Push changes
+        git push origin main
+    fi
 
     # Return to the root directory
     cd - || exit
